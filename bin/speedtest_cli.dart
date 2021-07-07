429824e51618727a33d92bb7c87bb53bfad7fa29
thawzin-11 <skydark.thawzintun@gmail.com>
2021-07-07 20:21:17 +0630

    first commit

---
+++ bin/speedtest_cli.dart
@@ -0,0 +1,6 @@
+import 'package:speedtest_cli/speedtest_cli.dart' as speedtest_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${speedtest_cli.calculate()}!');
+  print('Dummy');
+}
---
+++ test/speedtest_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:speedtest_cli/speedtest_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
